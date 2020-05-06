bfaa2516a27167cb8d0dc44c239cb48fe4cd6a91
ThibaudLamothe <thibaud.lamothe@capgemini.com>
2020-05-06 18:31:21 +0200

    add paste function and docstrings to default selenium

--- README.md
+++ README.md
@@ -15,13 +15,18 @@
 
 <center><img src="https://github.com/ThibaudLamothe/translate-pptx/blob/master/fig/translation_example.png?raw=true" alt="drawing" width="500" /></center>
 
+self.available_languages = ['fr', 'en', 'de', 'es', 'pt', 'it', 'nl', 'pl', 'ru', 'ja', 'zh']
 
 ## 3. PPT Insertion
 - Replacing text without modifying its look
 
 
 # Resources
-
 https://python-pptx.readthedocs.io/en/latest/
 https://github.com/scanny/python-pptx/issues/285
 https://stackoverflow.com/questions/1977362/how-to-create-module-wide-variables-in-python
+https://www.selenium.dev/documentation/fr/
+https://selenium-python.readthedocs.io/
+
+Chromedriver : http://chromedriver.storage.googleapis.com/index.html
+CSS Selectors : https://saucelabs.com/resources/articles/selenium-tips-css-selectors
--- src/default_selenium.py
+++ src/default_selenium.py
@@ -8,49 +8,118 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 class seleniumDefault:
+    """ Skeleton for apps using Selenium. Frequently needed shortcuts are available in here.
+    """
 
     def __init__(self, driver_path='chromedriver', loglevel='error'):
+        """Instanciate the class needs to feed a selenium driver.
+           (Development realized on macOS Catalina + Chrome)
         
-        self.driver_path = driver_path
+        PARAMETERS:
+        - driver_path : str - path to the selenium driver (mandatory)
+        - loglevel    : str - [debug/info/warning/error] (default is error). Adapt the level of information displayed in terminal.
+        """
+        
+        self.url = None
         self.original_window = None
         self.display_sleep_time=False
-        self.url = None
-
+        
+        self.driver_path = driver_path
         self.create_driver()
         self.set_loglevel(loglevel)
     
-    def create_driver(self):
-        logger.error(self.driver_path)
-        self.driver = webdriver.Chrome(self.driver_path)
 
-    def add_url(url):
-        self.url = url    
+    def create_driver(self):
+        """Instanciate 
+        """
+
+        logger.error(self.driver_path)
+        self.driver = webdriver.Chrome(self.driver_path)  # TODO:check path
+
+
+    def connect_to_url(self, url):
+        """ Redirect the webpage to a new url.
+        PARAMETERS:
+            - url : str - webpage to visit with webdriver.
+        """
+        self.url = url
+        self.load_url()  
+
 
     def load_url(self, sleep=3):
+        """Instantiate a new driver instance.
+
+        PARAMETERS:
+            - sleep : int - number of waiting seconds after instantiating driver. Default is 3.
+        """
+
         self.driver.get(self.url)
         self.original_window =  self.driver.window_handles[0]
         self.sleep(sleep, 'Load url : {}'.format(self.url))
 
+
+    def paste_clipboard(self, element, os='macos', driver='chrome'):
+        """ Copy clipboard content into the selected element
+
+        PARAMETERS:
+            - element : selenium element
+            - os      : str - not used
+            - driver  : strt - not used
+        
+        NO OUTPUT.
+        """
+
+        element.send_keys(Keys.SHIFT, Keys.INSERT)
+
+
     def scroll_to_element(self, elmt, sleep=1):
+        """Scroll to any defined element on the webpage (essentially to make it clickcable).
+        
+        PARAMETERS:
+            - elmt : selenium element - element to scroll to
+            - sleep : int - number of waiting seconds after scrolling. Default is 1.
+        """
+
         y = elmt.location['y']
         logger.debug('scrolling to y={}'.format(y))
         y = y - 150
         self.driver.execute_script("window.scrollTo(0, {})".format(y))
         self.sleep(sleep)
 
-    def sleep(self, sleep=1, message='Wait'):
+
+    def sleep(self, sleep=1, message='Wait', exact=False):
+        """ Sleeping function : as web pages might take time to react, it is interesting to wait for their responses.
+            To introduce noise the time value is slightly modified around the desired value.
+        PARAMETERS:
+            - sleep   : int - number of waiting seconds. Default is 1.
+            - message : message to display in terminal (in addition to the waiting time). Displayed only when loglevel is debug or info.
+            - exact   : boolean - To use the exact waitting value. Default is False: random noise of max 0.5s and minimum waiting time is 1s.
+        """
+
         if self.display_sleep_time:
             logger.info('({} s.) - {}'.format(sleep, message))
-        noise = random.random() - 0.5
-        sleep = max(sleep + noise, 1)
+        
+        if not exact:
+            noise = random.random() - 0.5
+            sleep = max(sleep + noise, 1)
         time.sleep(sleep)
 
+
     def close_driver(self):
+        """ Close the driver instance and the window associated.
+            Might be useful in case of bug or wrong instanciation.
+        """
+
         logger.error('Exit navigation.')
         self.driver.quit()
         
 
     def set_loglevel(self, level):
+        """ Adapting the loglevel for information display through process.
+
+        PARAMETERS:
+            - loglevel : str - [debug/info/warning/error]
+        """
         level_table = {
             'debug':logging.DEBUG,
             'warn':logging.WARNING,
